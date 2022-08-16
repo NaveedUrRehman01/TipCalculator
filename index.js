64f3d157e2ee9eff67bdcfabbf055064078108d9
= <=>
2022-08-16 14:46:37 +0500

    the app is completed

--- index.js
+++ index.js
@@ -1,56 +1,65 @@
 
- const bill = document.getElementById('billOrder').value;
- const numOfPeople  = document.getElementById('numOfPeople').value;
- const custom  = document.getElementById('custom').value;
+ let bill = document.getElementById('billOrder').value;
+ let numOfPeople  = document.getElementById('numOfPeople').value;
+ let custom  = document.getElementById('custom').value;
 
  function getBill(){
     
-    const billPerPerson = bill / numOfPeople;
-    const tip = billPerPerson * 0.05;
-    const totalBillPerPerson = billPerPerson + tip; 
+    var billPerPerson = bill / numOfPeople;
+    var tip = billPerPerson * 0.05;
+    var totalBillPerPerson = billPerPerson + tip; 
     document.getElementById('tipamount').innerHTML = "$" + tip.toFixed(2);
     document.getElementById('total').innerHTML = "$" + totalBillPerPerson.toFixed(2)
   
 }
 function getBill2(){
     
-    const billPerPerson = bill / numOfPeople;
-    const tip = billPerPerson * 0.10;
-    const totalBillPerPerson = billPerPerson + tip;
+    var billPerPerson = bill / numOfPeople;
+    var tip = billPerPerson * 0.10;
+    var totalBillPerPerson = billPerPerson + tip;
     document.getElementById('tipamount').innerHTML = "$" + tip.toFixed(2);
     document.getElementById('total').innerHTML = "$" + totalBillPerPerson.toFixed(2)
    
 }
 function getBill3(){
     
-    const billPerPerson = bill / numOfPeople;
-    const tip = billPerPerson * 0.15;
-    const totalBillPerPerson = billPerPerson + tip;
+    var billPerPerson = bill / numOfPeople;
+    var tip = billPerPerson * 0.15;
+    var totalBillPerPerson = billPerPerson + tip;
     document.getElementById('tipamount').innerHTML = "$" + tip.toFixed(2);
     document.getElementById('total').innerHTML = "$" + totalBillPerPerson.toFixed(2)
    
 }
 function getBill4(){
     
-    const billPerPerson = bill / numOfPeople;
-    const tip = billPerPerson * 0.25;
-    const totalBillPerPerson = billPerPerson + tip;
+    var billPerPerson = bill / numOfPeople;
+    var tip = billPerPerson * 0.25;
+    var totalBillPerPerson = billPerPerson + tip;
     document.getElementById('tipamount').innerHTML = "$" + tip.toFixed(2);
     document.getElementById('total').innerHTML = "$" + totalBillPerPerson.toFixed(2)
 }
 function getBill5(){
     
-    const billPerPerson = bill / numOfPeople;
-    const tip = billPerPerson * 0.50;
-    const totalBillPerPerson = billPerPerson + tip;
+    var billPerPerson = bill / numOfPeople;
+    var tip = billPerPerson * 0.50;
+    var totalBillPerPerson = billPerPerson + tip;
     document.getElementById('tipamount').innerHTML = "$" + tip.toFixed(2);
     document.getElementById('total').innerHTML = "$" + totalBillPerPerson.toFixed(2)
 }
 function CustomBill(){
     
-    const billPerPerson = bill / numOfPeople;
-    const tip = billPerPerson * custom / 100;
-    const totalBillPerPerson = billPerPerson + tip;
+    var billPerPerson = bill / numOfPeople;
+    var tip = billPerPerson * custom / 100;
+    var totalBillPerPerson = billPerPerson + tip;
     document.getElementById('tipamount').innerHTML = "$" + tip.toFixed(2);
     document.getElementById('total').innerHTML = "$" + totalBillPerPerson.toFixed(2)
+
+}
+function Reset(){
+     document.getElementById('billOrder').value = "0";
+      document.getElementById('numOfPeople').value = "0";
+      document.getElementById('custom').value = " ";
+      document.getElementById('tipamount').innerHTML = "$0.00"
+      document.getElementById('total').innerHTML = "$0.00" 
+     console.log(custom)
 }
